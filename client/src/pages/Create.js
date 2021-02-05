936fd701026d0dbf3c9b95cdcaa9046e69993c62
druloloy <dru.loloy@gmail.com>
2021-02-05 09:47:19 +0800

    Fixed Create.js and data reload

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -50,9 +50,9 @@ const Create = () =>{
                 
                 return axios.post('/history/add', history)
                 .then(()=>{
-                    return toast.success("Successfully added!", {position: "bottom-center", toastId: res.data._id})
+                    toast.success("Successfully added!", {position: "bottom-center", toastId: res.data._id})
+                    window.location = '/';
                 })
-            
             })
             .catch(e=>console.log(e));      
         } catch (e) {
